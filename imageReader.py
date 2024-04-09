f97643b87720f4b6c9f6a4f403d744ea5d8510f9
Shanvia Tech <shanviatechmac@gmail.com>
2024-04-09 10:28:36 +0530

    ReadImageText

---
+++ imageReader.py
@@ -0,0 +1,12 @@
+import os
+import pytesseract
+from PIL import Image
+
+directory = './images'
+files = os.listdir(directory)
+image_files = [file for file in files if file.endswith(('jpg', 'jpeg', 'png', 'gif', 'bmp','avif'))]
+for image_file in image_files:
+    image_path = os.path.join(directory, image_file)
+    image = Image.open(image_path)
+    text = pytesseract.image_to_string(image)
+    print(f"Text extracted from {image_file}:\n\n\n{text}\n\n\n")
